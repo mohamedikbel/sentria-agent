e2851d639392e964948e1b9881e7f7c8b823caf2
Ikbelsdiri <mohamedikbelsdiri@gmail.com>
2026-03-13 01:03:07 +0100

    first commit

---
+++ src/main/java/com/sentria/SentriaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sentria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentriaAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentriaAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sentria/SentriaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentriaAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
